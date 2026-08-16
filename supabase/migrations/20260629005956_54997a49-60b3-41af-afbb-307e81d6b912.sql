d71169a457b4f40c47edff64255f76c6316c87de
fernandasoaresanjos1-web <fernandasoaresanjos1@gmail.com>
2026-08-15 22:00:08 -0300

    copy: migration 20260629005956_54997a49-60b3-41af-afbb-307e81d6b912.sql

---
+++ supabase/migrations/20260629005956_54997a49-60b3-41af-afbb-307e81d6b912.sql
@@ -0,0 +1,33 @@
+DO $$
+DECLARE
+  tbl text;
+  pol record;
+  tables text[] := ARRAY[
+    'auditoria_dados',
+    'auditoria_sessao',
+    'auditoria_snapshots',
+    'agilizza_sessao',
+    'agilizza_cruzamento',
+    'agilizza_certificados',
+    'agilizza_cpfs',
+    'agilizza_extra',
+    'okr_estado'
+  ];
+BEGIN
+  FOREACH tbl IN ARRAY tables LOOP
+    IF to_regclass(format('public.%I', tbl)) IS NOT NULL THEN
+      EXECUTE format('ALTER TABLE public.%I DISABLE ROW LEVEL SECURITY', tbl);
+      FOR pol IN
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = 'public'
+          AND tablename = tbl
+      LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON public.%I', pol.policyname, tbl);
+      END LOOP;
+      EXECUTE format('GRANT SELECT, INSERT, UPDATE, DELETE ON public.%I TO anon', tbl);
+      EXECUTE format('GRANT SELECT, INSERT, UPDATE, DELETE ON public.%I TO authenticated', tbl);
+      EXECUTE format('GRANT ALL ON public.%I TO service_role', tbl);
+    END IF;
+  END LOOP;
+END $$;
